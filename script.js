63de917c6bf1e0e91a511439061958d86e5d5cf6
Kumang Subba <kumangsubba@gmail.com>
2024-02-17 11:36:54 +0200

    added keyboard support for buttons

--- script.js
+++ script.js
@@ -3,22 +3,35 @@ const screen = document.querySelector(".screen");
 const newCalculator = new Calculator();
 
 const allButtons = document.querySelectorAll("button");
-let screenValue = ""
-const buttonClick = (event) => {
-  const button = event.target.innerHTML;
+let screenValue = "";
+
+const SPECIAL_KEYS_MAP = {
+  C: "Escape",
+  "⌫": "Backspace",
+  "=": "Enter",
+  "×": "*",
+  "÷": "/",
+  "-": "-",
+};
+const handleButton = (button) => {
   newCalculator.handleButtons(button);
   screenValue = newCalculator.getDisplayValue();
-  updateScreen()
-
+  updateScreen();
+};
+const buttonClick = (event) => {
+  const button =
+    SPECIAL_KEYS_MAP[event.target.innerHTML] || event.target.innerHTML;
+  handleButton(button);
 };
 allButtons.forEach((button) => {
   button.addEventListener("click", buttonClick);
 });
-function updateScreen(){
-  screen.innerHTML = ""
-  for (let i = 0; i <screenValue.length; i++){
-    const digitSpan = document.createElement("span")
-    digitSpan.textContent = screenValue[i]
-    screen.appendChild(digitSpan)
+function updateScreen() {
+  screen.innerHTML = "";
+  for (let i = 0; i < screenValue.length; i++) {
+    const digitSpan = document.createElement("span");
+    digitSpan.textContent = screenValue[i];
+    screen.appendChild(digitSpan);
   }
 }
+document.addEventListener("keydown", (e) => handleButton(e.key));
